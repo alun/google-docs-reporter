f8b885fc96a2896215b105205a3585cdf976fb1a
Katherine Lunacharskaya <ea@katlex.com>
2011-01-04 09:24:13 +0600

    Taken user name and password to system properties

--- src/main/scala/com/katlex/reporter/GoogleDocs.scala
+++ src/main/scala/com/katlex/reporter/GoogleDocs.scala
@@ -8,27 +8,51 @@ import org.apache.http.conn.ConnectTimeoutException
 import java.net.UnknownHostException
 
 object GoogleDocs extends Application {
+  private def obtainUserAndPassword = {
+    val Array(user, passwd) = Array("google.user", "google.password").
+                                    map(System.getProperty)
+    if (user == null || passwd == null) {
+      println("""Please, specify user name and password
+        |for google connetion through java system properties
+        |-Dgoogle.user and -Dgoogle.password""".stripMargin)
+      System.exit(1)
+    }
+    (user, passwd)
+  }
+
   val transport = GoogleTransport.create
   transport.addParser(new JsonCParser)
-  try {
+  println()
+  val result = try {
+    val (user, passwd) = obtainUserAndPassword
+
     val a = new ClientLogin
     a.authTokenType = "wise"
-    a.username = "alexey.lunacharsky@stdva.com"
-    a.password = "x"
+    a.username = user
+    a.password = passwd
+    a.applicationName = "katlex-wise_client-1"
+    a.accountType = "HOSTED_OR_GOOGLE"
     a.authenticate.setAuthorizationHeader(transport)
 
     val rq = transport.buildGetRequest
     rq.setUrl("https://spreadsheets.google.com/feeds/spreadsheets/private/full")
-    println(rq.execute.parseAsString)
+    Left(rq.execute.parseAsString)
   } catch {
     case e: HttpResponseException =>
-      Console.err.println(e.response.parseAsString)
-      throw e
+      val info = e.response.parseAs(classOf[ClientLogin.ErrorInfo])
+      Right(info.error)
     case _: ConnectTimeoutException =>
-      Console.err.println("Host connetion is timedout")
-      System.exit(1)
+      Right("Host connetion is timed out")
     case e: UnknownHostException =>
-      Console.err.println("Unknown host: " + e.getMessage)
-      System.exit(1)
+      Right("Unknown host: " + e.getMessage)
   }
+
+  result match {
+    case Left(msg) => println(msg)
+    case Right(msg) =>
+      Console.err.println(msg)
+      System.exit(2)
+  }
+
+
 }
